6f55fdbfd6f9a44cfaa9a2eaeaa72647b15fe14c
pvk13797 <pvk13797@gmail.com>
2024-02-15 17:54:36 +0530

    Vercel Speed Insights Injected

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { IonicModule } from '@ionic/angular';
 import { HttpClientModule } from '@angular/common/http';
 
+
 @NgModule({
   declarations: [
     AppComponent
